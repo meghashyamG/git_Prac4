60482f75cfa271a2d0ecd09b9fc9cffc73953c18
shyam <shyamgodavari7@gmail.com>
2024-03-21 14:41:38 +0530

    initilisation

--- src/git_Prac2/Git.java
+++ src/git_Prac2/Git.java
@@ -3,10 +3,8 @@ package git_Prac2;
 public class Git {
 	public static void main(String[] args) {
 		System.out.println("hi there");
-		System.out.println("hi there, shyam is also cool");
-		System.out.println("typing is one of the things i need to improve");
-		System.out.println("good morning");
-		System.out.println("good evening");
-	}
+		System.out.println("hi there,dear 17 billion");
 
+
+}
 }
