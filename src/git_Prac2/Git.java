263a664b1849a93789be7dc1b160974ae4cf5426
shyam <shyamgodavari7@gmail.com>
2024-03-21 12:17:03 +0530

    develop branch changed code

--- src/git_Prac2/Git.java
+++ src/git_Prac2/Git.java
@@ -3,6 +3,10 @@ package git_Prac2;
 public class Git {
 	public static void main(String[] args) {
 		System.out.println("hi there");
+		System.out.println("hi there, shyam is also cool");
+		System.out.println("typing is one of the things i need to improve");
+		System.out.println("good morning");
+		System.out.println("good evening");
 	}
 
 }
